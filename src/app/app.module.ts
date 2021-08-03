2fdc65ff984e4532a4bc0bed5f0a9d20f509adb1
inqu <inqu16@gmail.com>
2021-08-03 23:03:50 +0900

    Fix: 설명 주석 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,15 @@ import { TimeDisplayComponent } from './section/time-display/time-display.compon
 
 
 @NgModule({ //앵귤러 제공 api 
-  declarations: [  //컴포넌트 들이 들어간다
+  declarations: [  //컴포넌트 들이 들어간다 - 하나의 모듈만 가능 의존성 주입 - 해당 컴포넌트를 앵귤러가 사용할 수 있게 해준다.
     AppComponent, HeaderComponent, FooterComponent, SectionComponent, ButtonsComponent, TimeDisplayComponent,
   ],
-  imports: [ // 다른 모듈들이 들어온다.
+  imports: [ // 다른 모듈들이 들어온다. import * as moduleName 과 같다.
     BrowserModule,
     AppRoutingModule
   ],
   providers: [], //뷰가 아닌 서비스 로직
-  bootstrap: [AppComponent] //처음 실행할 컴포넌트 지정
+  bootstrap: [AppComponent], //처음 실행할 컴포넌트 지정
+  exports:[] //컴포넌트 exoport list, es6 동일
 })
 export class AppModule { }
